0f66ee128a588ace51f0065f73327e78ebc7dd00
levrotech <hi.levrotech@tutanota.com>
2024-05-31 22:04:42 +0530

    Add Contact struct and initialize database connection
    - Introduced `Contact` struct with fields: name, number, email, and address.
    - Set up initial `main` function to establish a connection to the SQLite database at 'db/phonebook.db'.

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,14 @@
-fn main() {}
+use rusqlite::{Connection, Result};
+
+struct Contact {
+    name: String,
+    number: u32,
+    email: Option<String>,
+    address: Option<String>,
+}
+
+fn main() -> Result<()> {
+    let conn = Connection::new("db/phonebook.db")?;
+
+    Ok(())
+}
